0a55d8f2ed237f40657dbd6b6fa1d5108ef4579e
Yoan Diaz Cubas <jcuban90@gmail.com>
2018-07-13 06:54:35 -0400

    update

--- src/components/component.module.js
+++ src/components/component.module.js
@@ -2,13 +2,13 @@ import config from '../config/global.json';
 import loginComponent from './login/login.component';
 // import forgotComponent from './forgot/forgot.component';
 
-import forgotController from './modals/forgot/forgot.controller';
+import ForgotController from './modals/forgot/forgot.controller';
 
 const componentModule = angular.module('app.component', []);
 
 componentModule.constant("_path", config._path);
 
-componentModule.controller('forgotController', forgotController);
+componentModule.controller('ForgotController', ForgotController);
 
 // loading components, services, directives, specific to this module.
 componentModule.component('login', loginComponent);
