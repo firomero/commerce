d702b3688e2904f1ff01efd4a36f5b5c64e0ad01
Yoan Diaz Cubas <jcuban90@gmail.com>
2018-07-27 17:10:45 -0400

    add accounts

--- src/modules/investment/controller/investment.controller.js
+++ src/modules/investment/controller/investment.controller.js
@@ -1,4 +1,23 @@
-export default function InvestmentController($scope) {
+export default function InvestmentController($scope, userLogin) {
 	'ngInject';
+
+	$scope.currentCompany = { nameID: null, name: '', rol: '', accounts: [] };
+	$scope.loadAccounts = false;
 	
+	activate();
+	
+	function activate() {
+
+		$scope.currentCompany = { nameID: null, name: '', rol: '', accounts: [] };
+		$scope.currentCompany.nameID = userLogin.currentCompany;
+		for(var i = 0;i < userLogin.companies.length;i++) {
+
+			if (userLogin.companies[i].nameID == $scope.currentCompany.nameID) {
+				$scope.currentCompany.rol = userLogin.companies[i].rol;
+				$scope.currentCompany.name = userLogin.companies[i].name;
+				$scope.currentCompany.accounts = userLogin.companies[i].accounts;
+				break;
+			}
+		}
+	}	
 }
